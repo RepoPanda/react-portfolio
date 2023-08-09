7e2ca7f3566f29bd5ad688e1645d898a526cefd2
RepoPanda <124810474+RepoPanda@users.noreply.github.com>
2023-08-08 21:25:23 -0700

    Header is now rendering on all pages

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Portfolio from './pages/Portfolio';
 import About from './pages/About';
 import Resume from './pages/Resume';
 import Contact from './pages/Contact';
+import Header from './components/Header';
 
 export default function App() {
   const [currentPage, setCurrentPage] = useState('About');
@@ -26,6 +27,7 @@ export default function App() {
 
   return (
     <div>
+      <Header />
       {/* We are passing the currentPage from state and the function to update it */}
       <NavTabs currerntPage={currentPage} handlePageChange={handlePageChange} />
       {/* Here we are calling the renderPage method which will return a component  */}
