18c0b6d1ca506b177e44fdbd33ca520f136bd19e
RepoPanda <124810474+RepoPanda@users.noreply.github.com>
2023-08-11 22:37:55 -0700

    Header and Nav components styling had been applied.

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ export default function App() {
   return (
     <div>
       <Header>
-        <Nav currerntPage={currentPage} handlePageChange={handlePageChange} />
+        <Nav currerntPage={currentPage} handlePageChange={handlePageChange}></Nav>
       </Header>
       {renderPage()}
       <Footer />
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,28 @@
-function Header() {
+function Header({ children }) {
 	return (
-		<header>
-			<h1>
-				Welcome to my site.
-			</h1>
-		</header>
-	);
+    <header id="header">
+	  <h1>Philip J. Mejia</h1>
+      {/* <svg
+        version="1.1"
+        width="200"
+        height="50"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <rect x="0" y="0" width="200" height="100" fill="#08c17d" />
+        <text
+          fontFamily="Permanent Marker"
+          x="100"
+          y="34"
+          fontSize="26"
+          textAnchor="middle"
+          fill="#fffdd0"
+        >
+          Philip J. Mejia
+        </text>
+      </svg> */}
+      {children}
+    </header>
+  );
 }
 
 export default Header;
