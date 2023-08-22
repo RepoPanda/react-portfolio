0baaf79461717d2ad534e089c7d63d226cd33c4b
RepoPanda <124810474+RepoPanda@users.noreply.github.com>
2023-08-21 21:26:50 -0700

    contact form has been completed to display messages

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 import Button from 'react-bootstrap/Button';
@@ -8,6 +8,46 @@ import { validateEmail } from "../utils/helpers";
 
 
 export default function Contact() {
+
+  const [formState, setFormState] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
+
+  const [errorMessage, setErrorMessage] = useState('');
+  const { name, email, message } = formState;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!errorMessage) {
+      console.log('Submit Form', formState);
+    }
+  };
+
+  const handleChange = (e) => {
+    if (e.target.name === 'email') {
+      const isValid = validateEmail(e.target.value);
+      if (!isValid) {
+        setErrorMessage('Your email is invalid.');
+      } else {
+        setErrorMessage('');
+      }
+    } else {
+      if (!e.target.value.length) {
+        setErrorMessage(`${e.target.name} is required.`);
+      } else {
+        setErrorMessage('');
+      }
+    }
+    if (!errorMessage) {
+      setFormState({ ...formState, [e.target.name]: e.target.value });
+      console.log('Handle Form', formState);
+    }
+  };
+
+    
+
     return (
       <>
         <div class="ccard-container">
@@ -15,7 +55,7 @@ export default function Contact() {
             <h2>Contact Me</h2>
           </div>
 
-          <div class="form-group">
+          <form class="form-group" id="contact-form" onSubmit={handleSubmit}>
             <InputGroup className="mb-3">
               <InputGroup.Text id="inputGroup-sizing-default">
                 Name
@@ -23,11 +63,11 @@ export default function Contact() {
               <Form.Control
                 aria-label="Default"
                 aria-describedby="inputGroup-sizing-default"
+                type="text"
+                name="name"
+                defaultValue={name}
+                onBlur={handleChange}
               />
-              <Form.Text id="passwordHelpBlock" muted>
-                Your name can be 0-30 characters long, 
-                and must not contain any special characters.
-              </Form.Text>
             </InputGroup>
             <br />
             <InputGroup className="mb-3">
@@ -37,22 +77,33 @@ export default function Contact() {
               <Form.Control
                 aria-label="Default"
                 aria-describedby="inputGroup-sizing-default"
+                type="email"
+                name="email"
+                defaultValue={email}
+                onBlur={handleChange}
               />
-              <Form.Text id="passwordHelpBlock" muted>
-                Must be a valid email address.
-              </Form.Text>
             </InputGroup>
             <br />
             <InputGroup>
               <InputGroup.Text>Message</InputGroup.Text>
-              <Form.Control as="textarea" aria-label="With textarea" />
-              <Form.Text id="passwordHelpBlock" muted>
-                Please provide a message.
-              </Form.Text>
+              <Form.Control
+                as="textarea"
+                aria-label="With textarea"
+                name="message"
+                rows="5"
+                defaultValue={message}
+                onBlur={handleChange}
+              />
             </InputGroup>
             <br />
+            {errorMessage && (
+              <div>
+                <p className="error-text">{errorMessage}</p>
+              </div>
+            )}
+            <br />
             <Button as="input" type="submit" value="Submit" />{" "}
-          </div>
+          </form>
         </div>
       </>
     );
